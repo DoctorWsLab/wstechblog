e661ebbf611145c1c47968efc89d3a4c40602e98
W <w@w.com>
2022-01-17 15:25:10 +0900

    changes to blog contents

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ const Home = ({ allPostsData }) => {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello! I'm a software engineer.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
